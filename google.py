a46e6b71af0d75c0f20317bc55b1d401b4f4a308
Lachlan Cesca <lachlan.cesca@gmail.com>
2015-09-07 16:27:21 +1000

    Added functionality for multiple alternative routes. Added getters and count() for route and steps General trip information function

--- google.py
+++ google.py
@@ -9,39 +9,93 @@ API_KEY = "AIzaSyDXZvtLzLPExxM6NhpfAoHZLMV-gRRX66s"
 mapService  = Client(API_KEY)
 
 
-# Get Directions
+
+
+#--------------------------------Get Direction Information--------------------------------#
+
+# Get Directions between two locations
+# Param: mode allows specification of transport mode (Walking, Bicylcing, Driving, Transit)
+# When mode is specified, give alternative routes
+
 # TODO: Add in validity checks, catch exceptions, ect
 def get_directions(start_location, end_location, mode=None):
-    if mode is not None:
-        return mapService.directions(start_location, end_location, mode)[0]
+    if mode:
+        return mapService.directions(start_location, end_location, mode,alternatives=True)
     else:
-        return mapService.directions(start_location, end_location)[0]
+        return mapService.directions(start_location, end_location)
+
+
+#------------------------------------Route Information-----------------------------------#
+
+def get_route(directions,route=None):
+    if route:
+        return directions[route]
+    else:
+        return directions[0]
+
+def num_routes(directions):
+    return len(directions)
+
+
+#-------------------------------------Trip Information------------------------------------#
+
+#---------------------Trip Duration---------------------#
+def trip_duration_str(route):
+    return route['legs'][0]['duration']['text']
+
+def trip_duration_val(route):
+    return route['legs'][0]['duration']['value']
+
+#---------------------Trip Distance---------------------#
+def trip_distance_str(route):
+    return route['legs'][0]['distance']['text']
+
+def trip_distance_val(route):
+    return route['legs'][0]['distance']['value']
+
+#---------------------Arrival Time---------------------#
+def arrival_time_str(route):
+    return route['legs'][0]['arrival_time']['text']
+
+def arrival_time_val(route):
+    return route['legs'][0]['arrival_time']['value']
+
+#---------------------Departure Time---------------------#
+def departure_time_str(route):
+    return route['legs'][0]['departure_time']['text']
+
+def departure_time_val(route):
+    return route['legs'][0]['departure_time']['value']
+
+#---------------------Start Address---------------------#
+def start_address(route):
+    return route['legs'][0]['start_address']
+
+#---------------------End Address---------------------#
+def end_address(route):
+    return route['legs'][0]['end_address']
+
+
+#--------------------------------Steps of Journey Information------------------------------#
+def get_step(route, number):
+    return route['legs'][0]['steps'][number]
+
+def num_steps(route):
+    return len(route)
 
 # Print html encoded trip instruction list
+# TODO: Doesnt print substeps
 def print_instructions(directions):
-    for step in directions['legs'][0]['steps']:
+    for step in route['legs'][0]['steps']:
         print(step['html_instructions'])
 
 
-# Get leg of the journey
-def get_step(directions, number):
-    return directions['legs'][0]['steps'][number]
 
-#---------------------------Trip Duration---------------------------#
-def trip_duration_str(directions):
-    return directions['legs'][0]['duration']['text']
 
-def trip_duration_val(directions):
-    return directions['legs'][0]['duration']['value']
+#---------------------------Transit Information---------------------------#
 
-#---------------------------Trip Distance---------------------------#
-def trip_distance_str(directions):
-    return directions['legs'][0]['distance']['text']
-
-def trip_distance_val(directions):
-    return directions['legs'][0]['distance']['value']
-
-#---------------------------Train Information---------------------------#
+def travel_type(step):
+    return step['travel_mode']
 
 def step_transit_details_long_name(step):
     if(step['travel_mode']=='TRANSIT'):
@@ -56,11 +110,9 @@ def step_transit_details_short_name(step):
         return None
 
 
-print("----------------------------------------------------------------------")
-directions_transit = get_directions('Brisbane','Gold Coast','transit')
-train = get_step(directions_transit,3)
-print(step_transit_details_short_name(train))
+print("--------------------------------------------------------------------------------------")
+directions_transit = get_directions('Cavendish Road, Coorparoo','Southbank, Brisbane','transit')
+transit_route = get_route(directions_transit)
+transit_step = get_step(transit_route,1)
+travel_type(transit_step)
 
-
-#print_instructions(directions_transit)
-#print(trip_duration_str(directions_transit))
