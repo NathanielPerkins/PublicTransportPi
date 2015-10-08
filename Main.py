a727a14793c901e38a37669ddc4d561481491b83
Lachlan Cesca <lachlan.cesca@gmail.com>
2015-10-08 23:45:22 +1000

    Added in the beggining of a setup screen. The setup screen takes in 3 paramters via textbox prompts in order to connect to wifi. Those credentials will then be saved in the wpa_supplicant.
    TODO:
    The device will need to reboot once the wifi details have been attained
    and the device will have to check on startup if it is connected to wifi.

--- Main.py
+++ Main.py
@@ -1,17 +1,45 @@
 import Tkinter
+import Tk
 from GUIScreen import *
 
 w = 320
 h = 240
 
+SKIP_CHECK = 1
+
+def readConf():
+    f = open("setup.conf",'r+')
+    content = f.read()
+    SETUP = content[-2]
+    if(SETUP == "0"):
+        root = Tk.wifi()
+        f.seek(0, 0);
+        f.write("SETUP=1")
+    else:
+        root = Tkinter.Tk()
+    f.close()
+    return root
+if(SKIP_CHECK == 0):
+    root = readConf()
+else:
+    root = Tkinter.Tk()
+
 #Setup Tkinter GUI
-root = Tkinter.Tk()
+#root = Tkinter.Tk()
+
+#Final Layout    
+root.attributes('-fullscreen', True)
+
+#Testing Layout
 root.overrideredirect(1)
 root.geometry("%dx%d+0+0" % (w,h))
 #root.focus_set()
 
+
+
 canvas = Tkinter.Canvas(root,width=w,height=h)
 canvas.pack()
+
 
 
 # Create blank image for drawing.
@@ -25,6 +53,7 @@ draw = ImageDraw.Draw(image)
 newUI = UI(canvas,image, draw)
 
 try:
+    
     while 1:
         newUI.checkInputs()
         newUI.update()
---
+++ Tk.py
@@ -0,0 +1,102 @@
+from Tkinter import *
+
+
+filePath = "wpa_supplicant.conf"
+
+class Application(Frame):
+
+
+    def __init__(self, master=None):
+        master.attributes('-fullscreen', True)
+        self.w,self.h = 320,240
+        self.master = master
+        Frame.__init__(self, master)
+
+        self.pack()
+        self.createWindowOne()
+        self.window = 1
+
+
+    def keyPress(self,event):
+        self.button_callback()
+
+    def button_callback(self):
+        if(self.window == 1):
+            self.SSID = self.textBox.get("1.0",END)[:-1]
+            self.destroyWindow()
+            self.createWindowTwo()
+            self.window = 2
+        elif(self.window == 2):
+            self.SECURITY_TYPE = self.textBox.get("1.0",END)[:-1]
+            self.destroyWindow()
+            self.createWindowThree()
+            self.window = 3
+        elif(self.window == 3):
+            self.SECURITY_PASSWORD = self.textBox.get("1.0",END)[:-1]
+            self.destroyWindow()
+            #print("SSID: "+self.SSID+", Security Type: "+self.SECURITY_TYPE+", Password: "+self.SECURITY_PASSWORD)
+            self.destroy()
+            self.master.quit()
+            self.saveNetwork(self.SSID,self.SECURITY_TYPE,self.SECURITY_PASSWORD)
+
+    def createWindowOne(self):
+        self.label = Label(self, text="Enter SSID:")
+        self.label.pack()
+        
+        self.textBox = Text(self, height=2, width=self.w/2)
+        self.textBox.bind("<Return>",self.keyPress)
+        self.textBox.pack()
+
+
+        self.OK = Button(self,text="OK",command=self.button_callback)
+        self.OK.pack()
+        self.textBox.focus()
+        #self.OK.pack({"side": "left"})
+
+    def destroyWindow(self):
+        self.label.destroy()
+        self.textBox.destroy()
+        self.OK.destroy()
+
+    def createWindowTwo(self):
+
+        self.label = Label(self, text="Enter Secutiry Type:")
+        self.label.pack()
+        
+        self.textBox = Text(self, height=2, width=self.w/2)
+        self.textBox.bind("<Return>",self.keyPress)
+        self.textBox.pack()
+
+
+        self.OK = Button(self,text="OK",command=self.button_callback)
+        self.OK.pack()
+        self.textBox.focus()
+
+    def createWindowThree(self):
+
+        self.label = Label(self, text="Enter WiFI password:")
+        self.label.pack()
+        
+        self.textBox = Text(self, height=2, width=self.w/2)
+        self.textBox.bind("<Return>",self.keyPress)
+        self.textBox.pack()
+
+        self.OK = Button(self,text="OK",command=self.button_callback)
+        self.OK.pack()
+        self.textBox.focus()
+
+
+    def saveNetwork(self,ssid,keyType,key):
+        f = open(filePath,'a')
+        string = '\nnetwork{\n	ssid:"'+ssid+'"\n'
+        string+= '	key_mgmt='+keyType+'\n'
+        string+= '	psk="'+key+'"\n}'
+
+        f.write(string)
+        f.close()
+
+def wifi():
+    root = Tk()
+    app = Application(master=root)
+    app.mainloop()
+    return root
