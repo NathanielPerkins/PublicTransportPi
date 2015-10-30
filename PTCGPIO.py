7388ee9a5213a624eb665cfa97cb7630da617b66
Lachlan <lachlan.cesca@gmail.com>
2015-10-30 11:06:15 +0000

    Configured for device peripherals, LED's, Buttons, Speaker

--- PTCGPIO.py
+++ PTCGPIO.py
@@ -17,10 +17,29 @@ def buttonCheck(buttonMap):
             on.append(False)
     return on
         
-ledMap = []
-buttonMap = [12,14]
+ledMap = [21,20,16]
+buttonMap = [26,19,13,6]
 setup(ledMap,buttonMap)
 
+on = buttonCheck(buttonMap)
+GPIO.setup(12,GPIO.OUT)
+pwm = GPIO.PWM(12, 1000)
+
+while True:
+    on = buttonCheck(buttonMap)
+    
+    GPIO.output(ledMap[0],True)
+    GPIO.output(ledMap[1],False)
+    GPIO.output(ledMap[2],False)
+    
+    if(not on[0]):
+        GPIO.output(ledMap[1],True)
+    if(not on[1]):
+        GPIO.output(ledMap[2],True)
+    if(not on[2]):
+        pwm.start(50)
+    if(not on[3]):
+        pwm.stop()
 """
 print buttonCheck(buttonMap)
 while True:
